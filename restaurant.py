2e3c3762f2d27df8270bfdba542b7b5b3b841d33
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-01-29 00:17:48 +0600

    added restaurant class

---
+++ restaurant.py
@@ -0,0 +1,26 @@
+class Restaurant:
+    def __init__(self):
+        pass
+    
+    def master_pgd(self):
+        pass
+
+    def login(self, username, password):
+        pass
+
+    def admin_login(self):
+        pass
+
+    def staff_login(self):
+        pass
+
+    def logout_function(self):
+        pass
+
+    def add_user(self, admin_user, admin_pass):
+        pass
+
+    def log_file(self):
+        pass
+
+
